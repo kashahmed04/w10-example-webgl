be0d0f85e3ea3222638857ab6b14d2b8f6f046c1
Joe Pietruch <jcpigm@rit.edu>
2024-03-29 00:15:02 -0400

    Working BabylonJS

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,58 @@
 import './reset.css';
 import './styles.css';
 
-import * as THREE from 'three';
+import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera';
+import { Engine } from '@babylonjs/core/Engines/engine';
+import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
+import { Vector3 } from '@babylonjs/core/Maths/math.vector';
+import { CreateBox } from '@babylonjs/core/Meshes/Builders/boxBuilder';
+import { Scene } from '@babylonjs/core/scene';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, 800 / 600, 0.1, 1000);
+import { StandardMaterial, Texture } from '@babylonjs/core';
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(800, 600);
-document.body.prepend(renderer.domElement);
+// Get the canvas element from the DOM.
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+canvas.width = 800;
+canvas.height = 600;
 
-const texture = new THREE.TextureLoader().load('./tiger.png');
-texture.colorSpace = THREE.SRGBColorSpace;
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshPhysicalMaterial({
-  map: texture,
-  color: 0xffffff,
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Associate a Babylon Engine to it.
+const engine = new Engine(canvas);
 
-const ambient = new THREE.AmbientLight(0xaaaaaa, 0.25);
-scene.add(ambient);
+// Create our first scene.
+const scene = new Scene(engine);
 
-const light = new THREE.PointLight(0xffffff, 50, 100);
-light.position.set(1, 2, 3);
-scene.add(light);
+// This creates and positions a free camera (non-mesh)
+const camera = new FreeCamera('camera1', new Vector3(0, 0, -5), scene);
 
-camera.position.z = 2.5;
+// This targets the camera to scene origin
+camera.setTarget(Vector3.Zero());
 
-const animate = () => {
+// This attaches the camera to the canvas
+camera.attachControl(canvas, true);
+
+// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+const light = new HemisphericLight('light1', new Vector3(1, 1, 0), scene);
+
+// Default intensity is 1. Let's dim the light a small amount
+light.intensity = 1;
+
+// Create a grid material
+const material = new StandardMaterial('mat');
+const texture = new Texture('./tiger.png');
+material.diffuseTexture = texture;
+
+// Our built-in 'cube' shape.
+const cube = CreateBox('cube1', { size: 2 }, scene);
+
+// Move the cube upward 1/2 its height
+cube.position.y = 0;
+
+// Affect a material
+cube.material = material;
+
+// Render every frame
+engine.runRenderLoop(() => {
   cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-};
-
-animate();
+  cube.rotation.z += 0.01;
+  scene.render();
+});
