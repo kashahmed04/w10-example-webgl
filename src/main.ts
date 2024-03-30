6d0945300571c2627702a7f2982c7c384cf3ebe1
Kashaf Ahmed <kka7705@g.rit.edu>
2024-03-30 11:40:01 -0400

    Updated

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,26 @@ import './styles.css';
 
 import * as THREE from 'three';
 
+//go over all and what each part specificlly does**
+//how can we move camera is it mouse or by key**
+
+//so for three.js only we dont need a canvas HTML element and we can set canvas element up here**
+//we still need canvas element for main branch and babylon though**
+//is the main branch just webgl whereas three.js and babylon.js are webgl with libraries attached to webgl to make it easier to code**
+//why dont we say 'webgl' anywhere in here how does it know we are working with webgl with this library then**
 const scene = new THREE.Scene();
+//does scene represent making a canvas**
 
 const camera = new THREE.PerspectiveCamera(50, 800 / 600, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(800, 600);
 document.body.prepend(renderer.domElement);
+//we never defined DOM element**
 
 const texture = new THREE.TextureLoader().load('./tiger.png');
-texture.colorSpace = THREE.SRGBColorSpace;
+texture.colorSpace = THREE.SRGBColorSpace; //if we dont have this the whole canvas would look faded and this makes it look more cleaerer
+//and colorful**
 const material = new THREE.MeshPhysicalMaterial({
   map: texture,
   color: 0xffffff,
@@ -27,6 +37,8 @@ scene.add(ambient);
 const light = new THREE.PointLight(0xffffff, 50, 100);
 light.position.set(1, 2, 3);
 scene.add(light);
+
+//why did we define two lights here what does each do**
 
 camera.position.z = 2.5;
 
