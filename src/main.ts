bae80c452cf57d6e445890ab0bb596b7eea679ec
Kashaf Ahmed <kka7705@g.rit.edu>
2024-03-31 14:56:32 -0400

    Updated

--- src/main.ts
+++ src/main.ts
@@ -21,36 +21,46 @@ canvas.width = 800;
 canvas.height = 600;
 
 // Associate a Babylon Engine to it.
+// does this allow everything we apply from babylon.js library to apply in the scene only rather than the whole browser why did
+// we not do this in three.js or main branch**
 const engine = new Engine(canvas);
 
 // Create our first scene.
 const scene = new Scene(engine);
 
 // This creates and positions a free camera (non-mesh)
+// what does it mean a free camera and non-mesh**
 const camera = new FreeCamera('camera1', new Vector3(0, 0, -5), scene);
 
 // This targets the camera to scene origin
+// is the origin the middle of the screen or top left because we have canvas and canvas and JS are top left by default for origin**
+// where is origin in babylon.js and three.js and just webgl by default since they use canvas** 
 camera.setTarget(Vector3.Zero());
 
 // This attaches the camera to the canvas
+// what does the true do**
 camera.attachControl(canvas, true);
 
 // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+// what does the coorindates say because it says 1,1,0 instead**
 const light = new HemisphericLight('light1', new Vector3(1, 1, 0), scene);
 
 // Default intensity is 1. Let's dim the light a small amount
+// we still set it as 1 so how is it dimmed**
 light.intensity = 1;
 
 // Create a grid material
 const material = new StandardMaterial('mat');
 const texture = new Texture('./tiger.png');
 material.diffuseTexture = texture;
+//is diffuseTexture built in**
 
 // Our built-in 'cube' shape.
 const cube = CreateBox('cube1', { size: 2 }, scene);
 
 // Move the cube upward 1/2 its height
 cube.position.y = 0;
+//how did we know to make it 0 for the 1/2 of the height**
 
 // Affect a material
 cube.material = material;
@@ -61,3 +71,7 @@ engine.runRenderLoop(() => {
   cube.rotation.z += 0.01;
   scene.render();
 });
+
+//we dont have to do request animation frame because**
+//is this method built into babylon to run each frame (or is it the .render() that does the looping)**
+//does this work the same way as request animation frame**
